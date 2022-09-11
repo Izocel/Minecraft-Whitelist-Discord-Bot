98e5fd0fd592e2e53a53e8fd71f2750c402c653a
Izocel <triangle1232009@hotmail.com>
2022-09-11 09:47:53 -0400

    ...

--- build-WJE.bat
+++ build-WJE.bat
@@ -7,3 +7,7 @@ cd %BUILDER%
 call java --version
 call mvn --version
 call mvn -U clean install package -f %BUILDER%
+
+echo 
+echo Job's done look if BUILD SUCCES...
+set /p "id=</>Press enter to continue:"
