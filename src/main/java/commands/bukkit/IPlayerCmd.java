e77bc8cfbd9c1b36f67d18f2e33d283eacbfd25c
Kvasir.Qc <yvanlincroyable@gmail.com>
2023-02-05 15:59:25 -0500

    latest-stable osef

--- src/main/java/commands/bukkit/IPlayerCmd.java
+++
@@ -1,8 +0,0 @@
-package commands.bukkit;
-
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-
-public interface IPlayerCmd  {
-    abstract void execute(CommandSender sender, Command cmd, String label, String[] args);
-}
--- src/main/java/services/api/Api.java
+++
@@ -1,9 +0,0 @@
-package services.api;
-
-import java.net.URI;
-
-
-public class Api implements Iapi {
-    private URI baseUri;
-
-}
