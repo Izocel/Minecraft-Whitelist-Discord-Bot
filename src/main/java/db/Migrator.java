24df5362af8cd94f6c8f94f1703506cfaa3f4915
Raphael Vigneault Ð <triangle123209@hotmail.com>
2023-06-18 11:12:08 -0400

    DB Migrator - awaiting compatibility for flyway (issue)

--- src/main/java/db/Migrator.java
+++ src/main/java/db/Migrator.java
@@ -4,7 +4,6 @@ import java.util.logging.Logger;
 import org.flywaydb.core.Flyway;
 import org.flywaydb.core.api.configuration.FluentConfiguration;
 
-import configs.ConfigManager;
 import dao.DaoManager;
 import io.sentry.ISpan;
 import io.sentry.SpanStatus;
@@ -20,36 +19,26 @@ public class Migrator {
      * @param plugin
      */
     public Migrator(WhitelistJe plugin) {
+        ISpan process = plugin.getSentryService().findWithuniqueName("onEnable")
+                .startChild("Migrator");
+
+        this.plugin = plugin;
+        this.logger = Logger.getLogger("WJE:" + getClass().getSimpleName());
         try {
-            ISpan process = plugin.getSentryService().findWithuniqueName("onEnable")
-                    .startChild("Migrator");
-
-            this.plugin = plugin;
-            this.logger = Logger.getLogger("WJE:" + getClass().getSimpleName());
-
-            final ConfigManager configs = this.plugin.getConfigManager();
-
-            final String JDBC_USER = configs.get("dbUser");
-            final String JDBC_PASS = configs.get("dbPass");
-            final String JDBC_URL = configs.get("dbJdbcUrl");
-
-            try {
-                FluentConfiguration flywayConfigs = Flyway.configure()
+            FluentConfiguration flywayConfigs = Flyway.configure()
                 .dataSource(DaoManager.getDatasource())
                 .locations("classpath:db/migration");
 
-                this.flyway = new Flyway(flywayConfigs);
-                flyway.migrate();
+            this.flyway = new Flyway(flywayConfigs);
+            flyway.migrate();
 
-            } catch (Exception e) {
-                this.logger.warning(e.getMessage());
-                e.printStackTrace();
-            }
-
-            process.setStatus(SpanStatus.OK);
-            process.finish();
         } catch (Exception e) {
-            SentryService.captureEx(e);
+            this.logger.warning(e.getMessage());
+            e.printStackTrace();
+            //SentryService.captureEx(e);
         }
+
+        process.setStatus(SpanStatus.OK);
+        process.finish();
     }
 }
