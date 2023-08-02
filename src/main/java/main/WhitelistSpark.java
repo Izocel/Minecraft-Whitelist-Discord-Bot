875788cb8d658efc43c7cd88b065d15987cc0331
Raphael Vigneault Ð <triangle123209@hotmail.com>
2023-08-01 20:33:50 -0400

    spark basic example

--- src/main/java/main/WhitelistSpark.java
+++ src/main/java/main/WhitelistSpark.java
@@ -1,6 +1,8 @@
 package main;
 
+import static spark.Spark.before;
 import static spark.Spark.get;
+import static spark.Spark.path;
 import static spark.Spark.port;
 import static spark.Spark.secure;
 import static spark.Spark.staticFiles;
@@ -10,12 +12,16 @@ import java.util.logging.Logger;
 
 import org.apache.commons.io.FileUtils;
 
+import com.google.gson.JsonObject;
+
 import configs.ConfigManager;
 import helpers.FileHelper;
+import helpers.Helper;
+import services.sentry.SentryService;
 
 public class WhitelistSpark {
 
-    static Logger logger = Logger.getLogger("WDMC-Spark:" + WhitelistSpark.class.getSimpleName());
+    static Logger logger = Logger.getLogger("WDMC-API");
 
     public static void main(ConfigManager configs) {
         final String apiKey = configs.get("api.apiKey");
@@ -60,10 +66,51 @@ public class WhitelistSpark {
             File rootDir = new File(appRootPath);
             FileUtils.forceMkdir(rootDir);
             staticFiles.externalLocation(appRootPath);
-
-            get("/ping", (req, res) -> "Discord server id: " + discordId);
         } catch (Exception e) {
             logger.warning(e.getMessage());
         }
+
+        get("/ping", (req, res) -> "Discord server id: " + discordId);
+        setApiPaths(configs);
+    }
+
+    public static void setApiPaths(ConfigManager configs) {
+
+        path("/api", () -> {
+            before("/*", (req, res) -> {
+                req.session(true);
+                res.type("application/json");
+            });
+
+            path("/server", () -> {
+                before("/*", (req, res) -> logger.info("Received an api call [server]:"));
+
+            });
+            path("/users", () -> {
+                before("/*", (req, res) -> logger.info("Received a api call [users]:"));
+                get("/:id", (req, res) -> {
+                    final JsonObject data = new JsonObject();
+                    try {
+                        final Integer id = Helper.asInteger(req.params(":id"), 0);
+
+                        data.addProperty("id", id);
+                        data.addProperty("time", Helper.getSystemDate().toString());
+                    } catch (Exception e) {
+                        SentryService.captureEx(e);
+                        res.status(500);
+                        data.addProperty("error", "Server Error");
+                    }
+
+                    res.body(data.toString());
+                    return res.body();
+                });
+
+            });
+            path("/players", () -> {
+                before("/*", (req, res) -> logger.info("Received a api call [players]:"));
+
+            });
+        });
+
     }
 }
