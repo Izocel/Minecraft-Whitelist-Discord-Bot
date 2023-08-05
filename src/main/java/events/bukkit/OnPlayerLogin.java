b92a60321ea730c29ac27d53b9ea4909ffe35934
Raphael Vigneault Ð <triangle123209@hotmail.com>
2023-08-05 13:12:15 -0400

    ...

--- src/main/java/events/bukkit/OnPlayerLogin.java
+++ src/main/java/events/bukkit/OnPlayerLogin.java
@@ -147,8 +147,8 @@ public class OnPlayerLogin implements Listener {
             }
 
             else if (bedData != null) {
-                javaData.setAvatarUrl("https://api.tydiumcraft.net/v1/players/skin?uuid=" + "&size=72&type=head/"
-                        + loginPlayer.getUniqueId().toString() + "/avatarpic-l.png");
+                javaData.setAvatarUrl("https://api.tydiumcraft.net/v1/players/skin?uuid="
+                        + loginPlayer.getUniqueId().toString() + "&size=72&type=head/");
                 bedData.setMcName(PlayerDbApi.getXboxPseudo(loginPlayer.getUniqueId().toString()));
                 bedData.save(DaoManager.getBedrockDataDao());
             }
